7700434c7a3b0fe1d89433828f876055c9f6e0c2
Terez Ivy <terezivy@gmail.com>
2019-03-21 18:03:55 -0400

    Added a convenience method to the ServerLifeCycle interface to retrieve all the protocol to port mapping for use in Bundles in the event the ports are not declared in the config. The intention is to be able to use this in registration with tools like Consul. Created a new class, PortDescriptor, to be able to associate the port, protocol and the port type(application or admin).

---
+++ dropwizard-lifecycle/src/test/java/io/dropwizard/lifecycle/ServerLifecycleListenerTest.java
@@ -0,0 +1,18 @@
+package io.dropwizard.lifecycle;
+
+import org.junit.Test;
+
+public class ServerLifecycleListenerTest {
+
+    @Test
+    public void getLocalPort() {
+    }
+
+    @Test
+    public void getAdminPort() {
+    }
+
+    @Test
+    public void getPortDescriptorList() {
+    }
+}
